3b8e78377343d2e30e4ef5892d5d417a14ad53dc
rjan939 <rjan@micds.org>
2024-08-28 20:57:58 -0500

    Added EPA01 and finished PA02

---
+++ PA02/PA02.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+
+#define RANGE 10 
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    if (argc == 1) {
+        cerr << "Usage: ./PA02 filename" << "\n";
+        return 1;
+    }
+
+    string filename = argv[1];
+    ifstream file(filename);
+
+    if (!file) {
+        cerr << "Usage ./PA02 filename" << "\n";
+        return 2;
+    }
+
+    int counter[RANGE] = {0};
+    for (int i = 0; !file.eof(); i++) {
+        int x = 0;
+        file >> x;
+        if (x >= RANGE || x < 0) {
+            cerr << x << "not in range";
+            return 3;
+        } 
+        counter[x]++;
+    } 
+    file.close();
+    
+    for (int i = 0; i < RANGE; i++) {
+        cout << i <<  " " << counter[i] << "\n";
+    }
+
+    return EXIT_SUCCESS;
+}
